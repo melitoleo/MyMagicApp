7a895c8072985aeb5515546313ec388e2fcca58e
melitol <leonardo.melito.external@ynap.com>
2020-01-08 18:33:58 +0100

    add password strength

--- app/src/main/java/com/example/mymagicapp/RegistrationActivity.java
+++ app/src/main/java/com/example/mymagicapp/RegistrationActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.mymagicapp.dao.AccountDatabase;
 import com.example.mymagicapp.domain.Type;
@@ -20,6 +23,8 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText email;
     private EditText username;
     private EditText password;
+    private TextView txtPwStrength;
+    private View root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +38,17 @@ public class RegistrationActivity extends AppCompatActivity {
 
         final String[] defaultTypeDescription = {"Social","Email","a","b","c","d","e"};
 
+        root = findViewById(R.id.root);
+        name = findViewById(R.id.txtName);
+        surname = findViewById(R.id.txtSurname);
+        email = findViewById(R.id.txtMail);
+        username = findViewById(R.id.txtUsername);
+        password = findViewById(R.id.txtPassword);
+        txtPwStrength = findViewById(R.id.txtPwStrength);
+
         btnRegistration.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 User user = new User();
-
-                name = findViewById(R.id.txtName);
-                surname = findViewById(R.id.txtSurname);
-                email = findViewById(R.id.txtMail);
-                username = findViewById(R.id.txtUsername);
-                password = findViewById(R.id.txtPassword);
 
                 user.name = name.getText().toString();
                 user.surname = surname.getText().toString();
@@ -62,5 +69,28 @@ public class RegistrationActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calculatePasswordStrength(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+        });
+    }
+    private void calculatePasswordStrength(String str) {
+        // Now, we need to define a PasswordStrength enum
+        // with a calculate static method returning the password strength
+        PasswordStrength passwordStrength = PasswordStrength.calculate(str);
+        txtPwStrength.setText(passwordStrength.msg);
+        root.setBackgroundColor(passwordStrength.color);
     }
 }
